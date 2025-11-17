c8fc37d2ae38528745f664a9b8bb028792ba7cef
Miguel Sousa <miguelsoaressousa@ua.pt>
2025-11-17 00:01:21 +0000

    http e logger

---
+++ src/http.h
@@ -0,0 +1,15 @@
+#ifndef HTTP_H
+#define HTTP_H
+
+#include <stddef.h> 
+
+typedef struct {
+    char method[16];
+    char path[512];
+    char version[16];
+} http_request_t;
+
+int parse_http_request(const char* buffer, http_request_t* req);
+void send_http_response(int fd, int status, const char* status_msg, const char* content_type, const char* body, size_t body_len);
+
+#endif
